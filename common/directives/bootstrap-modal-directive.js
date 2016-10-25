37a57f652a2cfcd6ec22b56fd0a3abacac3cdc0f
Asraf Uddin Ahmed <13ratul@gmail.com>
2016-10-25 12:04:54 +0600

    bootstrap-modal-directive templates file added

--- common/directives/bootstrap-modal-directive.js
+++ common/directives/bootstrap-modal-directive.js
@@ -1,17 +1,7 @@
 
 angular.module('MyApp.directives').directive('bootstrapModal', function () {
     return {
-      template: '<div class="modal fade">' +
-          '<div class="modal-dialog">' +
-            '<div class="modal-content">' +
-              '<div class="modal-header">' +
-                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-                '<h4 class="modal-title">{{ title }}</h4>' +
-              '</div>' +
-              '<div class="modal-body" ng-transclude></div>' +
-            '</div>' +
-          '</div>' +
-        '</div>',
+      templateUrl: 'templates/bootstrap-modal-directive.html',
       restrict: 'E',
       transclude: true,
       replace:true,
